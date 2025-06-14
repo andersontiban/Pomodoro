a0655e11dd223296b668ddfe7c4a425d1ee545b0
Anderson Tiban <andersontiban@Andersons-MacBook-Air-2.local>
2025-06-14 10:41:04 -0400

    clean up

--- utils/stripe.js
+++
@@ -1,5 +0,0 @@
-import Stripe from "stripe"
-
-const key = "sk_test_51RLoYKPufkxpdRkozkqjz4EGU8ksfMsLKmSR2La6svJz7KYTV5LnCbqqfxm4RdBZTBAcVpLLkO5lXeAQdUa6zg9h008Qf5PSeQ"
-
-export const stripe = new Stripe(key)
